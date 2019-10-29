7218fc5257c5d5b4c4444b1caf843dc12e98360a
Kevin Browder <browderkevin54@gmail.com>
2019-10-28 22:49:47 -0600

    Added recusive comparison

--- JSONComparision/src/jsoncomparision/Client.java
+++ JSONComparision/src/jsoncomparision/Client.java
@@ -10,39 +10,50 @@ import com.google.gson.JsonElement;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
-
-/**
- *
+/** 
+ * Client class gets file paths from user, reads in JSONs and calls compare
  * @author Kevin
  */
 public class Client {
 
     /**
+     * Client class gets file paths from user, reads in JSONs and calls compare
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         importJson();
-        
+
     }
-    private static void importJson(){
-        final String JSON_PATH = "BreweriesMaster.json";
-        final String JSON_PATH1 = "BreweriesSample2.json";
+    /**
+     * Method gets file locations from user, reads in JSONs and compares them using
+     * Compare class
+     */
+    private static void importJson() {
+        //get json file locations 
+        Scanner scanner = new Scanner(System.in);  
+        System.out.println("Enter path to first file");
+        final String JSON_Master = scanner.nextLine();  
         
-        try{
-           
+        System.out.println("Enter path to second file");
+        final String JSON_Child = scanner.nextLine();
+        //read JSON files in using GSON
+        try {
+
             Gson gson = new Gson();
-            BufferedReader br = new BufferedReader(new FileReader(JSON_PATH));
-            BufferedReader br1 = new BufferedReader(new FileReader(JSON_PATH1));
-            
+            BufferedReader br = new BufferedReader(new FileReader(JSON_Master));
+            BufferedReader br1 = new BufferedReader(new FileReader(JSON_Child));
+
             JsonElement master = gson.fromJson(br, JsonElement.class);
             JsonElement child = gson.fromJson(br1, JsonElement.class);
             CompareJSON compare = new CompareJSON(master, child);
-            System.out.println(compare.Compare());
-        }
-        catch(FileNotFoundException e){
+            //call comparison on JSON files
+            compare.Compare();
+            
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
-        }  
+        }
     }
 }
--- JSONComparision/src/jsoncomparision/CompareJSON.java
+++ JSONComparision/src/jsoncomparision/CompareJSON.java
@@ -5,42 +5,104 @@
  */
 package jsoncomparision;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.util.Set;
 
 /**
- *
+ * Class takes two JSON elements and compares their similarity and outputs results to console
  * @author Kevin
  */
 public class CompareJSON {
+
+    //global declaration of JSON elements
     private JsonElement master;
     private JsonElement child;
-    
-    CompareJSON(JsonElement master, JsonElement child){
+
+    //global variables to keep count during recursive iteration in compare
+    private int iterations;
+    private int differences;
+
+    /**
+     * Constructor takes in two JSON elements and assigns them to global
+     * variables
+     * @param master
+     * @param child
+     */
+    CompareJSON(JsonElement master, JsonElement child) {
         this.master = master;
         this.child = child;
     }
-    
-    public double Compare(){
-        if(master.equals(child))
-        {
-            return 1;
-        }
-        else if(master.isJsonObject())
-        {
-            JsonObject masterobj = master.getAsJsonObject();
-            Set<String> keys = masterobj.keySet();
-            System.out.println(keys);
-            for(String key : keys){
-                System.out.println(masterobj.get(key));
-            }
-        }
-        double similarity = 0;
-        
-        return similarity;
+
+    /**
+     * calls recursive method and outputs results to console runs recursion
+     * twice to compare master to child and child to master
+     */
+    public void Compare() {
+        CompareRecursive(master, child);
+        CompareRecursive(child, master);
+        double similarity = ((double) iterations - (double) differences) / (double) iterations;
+        iterations = 0;
+        differences = 0;
+        System.out.println("The files have a similarity of " + similarity);
     }
 
+    /**
+     * Recursive method compares differences between MASTER and CHILD this is a
+     * one directional comparison from MASTER to CHILD Iterates recursively
+     * until a primitive data type is found then compares for similarity and add
+     * to running total
+     * @param master
+     * @param child
+     */
+    private void CompareRecursive(JsonElement master, JsonElement child) {
+        if (master.isJsonObject()) {
+            if (child == null) {
+                iterations++;
+                differences++;
+            } //if masters is object call recursivily on each of objects elements
+            else {
+                JsonObject masterobj = master.getAsJsonObject();
+                JsonObject childobj = child.getAsJsonObject();
+                Set<String> masterkeys = masterobj.keySet();
+                for (String key : masterkeys) {
+                    CompareRecursive(masterobj.get(key), childobj.get(key));
+                }
+            }
+        } else if (master.isJsonArray()) {
+            if (child == null) {
+                iterations++;
+                differences++;
+            } //if master is array, then compare 
+            else {
+                JsonArray masterarray = master.getAsJsonArray();
+                JsonArray childarray = child.getAsJsonArray();
+                int runs;
+                //if more elements in master array than child
+                if (childarray.size() < masterarray.size()) {
+                    runs = childarray.size();
+                    //add iterations and differences equal to number of extra elements in master array
+                    iterations += masterarray.size() - childarray.size();
+                    differences += masterarray.size() - childarray.size();
+                } else {
+                    runs = masterarray.size();
+                }
+                for (int i = 0; i < runs; i++) {
+                    CompareRecursive(masterarray.get(i), childarray.get(i));
+                }
+            }
+        } else if (master.isJsonPrimitive()) {
+            if (master.equals(child)) {
+                iterations++;
+            } else {
+                iterations++;
+                differences++;
+            }
+        }
+    }
+
+    //setters to change JSON files and compare different files
     public void setMaster(JsonElement master) {
         this.master = master;
     }
@@ -48,5 +110,5 @@ public class CompareJSON {
     public void setChild(JsonElement child) {
         this.child = child;
     }
-      
+
 }
